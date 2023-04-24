e97760f6b047d80a30512a4ab8a18cf5ff487ff6
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-24 19:33:08 -0300

    variaveis e tipos de dados

---
+++ Logica-de-Programacao-com-Java/introducao-a-linguagem-java/variaveis-e-tipos-de-dados/Main.java
@@ -0,0 +1,17 @@
+// Números inteiros -> int
+// Números flutuantes -> float
+// Textos entre aspas -> String
+// Verdadeiro ou falso -> Boolean
+class Main {
+  public static void main(String[] args) {
+    int id = 01;
+    float balance = (float) 123.98;
+    String name = "Josan Johnata";
+    Boolean isClient = true;
+
+    System.out.println("Id: " + id);
+    System.out.println("Client name: " + name);
+    System.out.println("balance: " + balance);
+    System.out.println("Is client? " + isClient);
+  }
+}
