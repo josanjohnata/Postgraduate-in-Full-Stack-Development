0fe4b2722b165757ec86271dadb75086c0e52742
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-24 22:08:12 -0300

    Programacao sequencial

---
+++ Logica-de-Programacao-com-Java/introducao-a-linguagem-java/programacao-sequencial/Main.java
@@ -0,0 +1,13 @@
+// Programação sequencial segue a sequencia de cascata, logo o programa é lido linha por linha na ordem de cima para baixo e da direita para esquerda.
+
+class Main {
+  public static void main(String[] args) {
+    double base, height, area;
+
+    base = 10.0;
+    height = 8.0;
+
+    area = (base * height) / 2;
+    System.out.println(area);
+  }
+}
