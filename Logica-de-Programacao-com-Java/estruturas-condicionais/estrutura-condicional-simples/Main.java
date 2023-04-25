b45690296777488c63e48668213f2c8449149181
Josan Johnata <josan.pontes@meliuz.com.br>
2023-04-25 20:09:43 -0300

    Estruturas condicional aninhado

--- Logica-de-Programacao-com-Java/estruturas-condicionais/estrutura-condicional-simples/Main.java
+++ Logica-de-Programacao-com-Java/estruturas-condicionais/estrutura-condicional-simples/Main.java
@@ -5,6 +5,8 @@ class Main {
 
     if(grade >= 6) {
       System.out.println("Aluno APROVADO!");
+    } else if(grade >= 4 && grade < 6) {
+      System.out.println("Aluno em RECUPERAÇÃO!");
     } else {
       System.out.println("Aluno REPROVADO!");
     }
