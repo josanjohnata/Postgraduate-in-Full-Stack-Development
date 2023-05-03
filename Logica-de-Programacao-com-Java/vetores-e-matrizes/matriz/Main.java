58c98a47432da854e0c1a5f798e6fa75f0a19a17
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-03 11:14:13 -0300

    Vetores e Matrizes - matrizes

---
+++ Logica-de-Programacao-com-Java/vetores-e-matrizes/matriz/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int[][] dados = new int[3][3];
+
+    for(int i = 0; i < 3; i++)
+      for(int j = 0; j < 3; j++)
+        dados[i][j] = j;
+
+    for(int i = 0; i < 3; i++)
+      for(int j = 0; j < 3; j++)
+        System.out.println(dados[i][j]);
+  }
+}
