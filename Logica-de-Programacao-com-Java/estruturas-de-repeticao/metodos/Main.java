4db3a5a8bcea57850738d6dd77d9dfb3409cf354
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-02 21:16:31 -0300

    Estruturas de repetição - Método

---
+++ Logica-de-Programacao-com-Java/estruturas-de-repeticao/metodos/Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static int sum(int a, int b) {
+    return (a + b);
+  }
+
+  public static void main(String[] args) {
+    int total = Main.sum(10, 20);
+    System.out.println(total);
+  }
+}
