4cba0d9d1829a804fbde917792db9e03940e004c
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-03 11:01:21 -0300

    Vetores e Matrizes - vetores

---
+++ Logica-de-Programacao-com-Java/vetores-e-matrizes/vetores/Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    int[] dados = new int[5];
+
+    dados[2] = 9;
+    dados[3] = 7;
+    dados[0] = 5;
+
+    System.out.println(dados[2]);
+  }
+}
