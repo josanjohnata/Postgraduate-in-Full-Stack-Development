f228b29e2925b30c12ae81d86e5453136b8017bb
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-10 20:14:30 -0300

    Introduçao a programação orientada a objetos - Conceitos de orientação a objetos

---
+++ Logica-de-Programacao-com-Java/introducao-a-programacao-orientada-a-objetos/conceitos-de-orientacao-a-objetos/Main.java
@@ -0,0 +1,24 @@
+// Conceitos de Orientação a Objetos
+
+/*
+ * Programação estruturada
+ *  Dados globais -> orientada a procedimentos
+ * 
+ * Programação orientada a objetos
+ *  Dados objetos -> atributos e métodos
+ * 
+ * Princípios essenciais:
+ *  - Abstração;
+ *  - Classes e objetos;
+ *  - Atributos, Métodos e Mensagens;
+ *  - Herança e Associação;
+ *  - Encapsulamento;
+ *  - Polimorfismo;
+ *  - Interfaces.
+ */
+
+ class Main {
+  public static void main(String[] args) {
+    
+  }
+ }
