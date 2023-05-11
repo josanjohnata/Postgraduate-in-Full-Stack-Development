8ebc2d23cccecc4b08965e3d5ab1d2eac40034e3
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-10 21:38:36 -0300

    Introduçao a programação orientada a objetos - Encapsulamento

---
+++ Logica-de-Programacao-com-Java/introducao-a-programacao-orientada-a-objetos/encapsulamento/Main.java
@@ -0,0 +1,11 @@
+// Encapsulamento
+/*
+ * Encapsular significa separar o programa em partes, em cápsulas autocontidas, tornando-o mais flexível.
+ * Desta forma:
+ * - promove-se a reutilização de código e a segurança;
+ * - o programa se torna mais fácil de modificar e de dar manutenção.
+ * 
+ * Métodos para trabalharmos com encapsulamento de dados:
+ * - setAtributo(parametro) -> Métodos set são conhecidos como métodos modificadores.
+ * - getAtributo() -> Métodos get são conhecidos como métodos de acesso.
+ */
---
+++ Logica-de-Programacao-com-Java/introducao-a-programacao-orientada-a-objetos/encapsulamento/Student.java
@@ -0,0 +1,11 @@
+public class Student{
+  private String name;
+
+  public void setName(n){
+    name = n;
+  }
+
+  public String getName(){
+    return name;
+  }
+}
