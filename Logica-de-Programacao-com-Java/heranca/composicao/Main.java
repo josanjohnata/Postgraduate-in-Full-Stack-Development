93faacdd9cbea73b83de4846e8d19149d59ad35b
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-14 15:20:15 -0300

    Herança - Composição

---
+++ Logica-de-Programacao-com-Java/heranca/composicao/Main.java
@@ -0,0 +1,14 @@
+// Composição
+/*
+ * Uma instância da classe existente é usada como componente da outra classe
+ * 
+ * Dizemos então que o relacionamento de Composição define um relacionamento do tipo "tem um":
+ * - "Bicicleta TEM UMA roda"
+ * - "Caminhão TEM UM motorista"
+ * - "Pedido TEM UM item"
+ */
+class Main {
+  public static void main(String[] args) {
+    Order o - new Order();
+  }
+}
---
+++ Logica-de-Programacao-com-Java/heranca/composicao/Order.java
@@ -0,0 +1,7 @@
+public class Order{
+  private Items i;
+
+  public Order() {
+    i = new Items();
+  }
+}
