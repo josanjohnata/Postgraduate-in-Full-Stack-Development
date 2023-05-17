194c71f8bfc56894790d399ed4dda1e7e8357960
Josan Johnata <josan.pontes@meliuz.com.br>
2023-05-17 19:08:32 -0300

    Collections - Saiba mais

---
+++ Logica-de-Programacao-com-Java/collections/saiba-mais/Main.java
@@ -0,0 +1,12 @@
+// Saiba mais
+/*
+ * A documentação Java tem todos as informações necessárias para tornar seu projeto um sucesso estrondoso.
+ * Documentação do JDK (Java SE)
+ * - https://docs.oracle.com/en/java/javase/19/
+ * 
+ * DocumentaçãoAPI:
+ * - https://docs.oracle.com/en/java/javase/19/docs/api/index.html
+ * 
+ * Documentação do Java EE
+ * - https://docs.oracle.com.javaee/7/index.html
+ */
