d7614a9108062ff22254f186a41d659db84f7fc0
Josan Johnata <josan.pontes@meliuz.com.br>
2023-06-01 21:38:14 -0300

    Intermediary Front end - Programação com JavaScript - Conceitos

---
+++ Intermediary-Front-End/JavaScript/main.js
@@ -0,0 +1,12 @@
+console.log('Hello world!');
+var a = 10;
+var b = 20;
+var total = a + b;
+console.log(total);
+
+var nomePessoal = "Josan";
+console.log(nomePessoal);
+
+let book = "A litter prince"
+
+const PI = 3.14;
