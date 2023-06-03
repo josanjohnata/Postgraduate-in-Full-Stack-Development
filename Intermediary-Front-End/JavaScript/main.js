b82f690658939ad2a96eb0109b99c45e8edae42b
Josan Johnata <josan.pontes@meliuz.com.br>
2023-06-02 21:43:33 -0300

    Intermediary Front end - Tipos de Dados e operadores

--- Intermediary-Front-End/JavaScript/main.js
+++ Intermediary-Front-End/JavaScript/main.js
@@ -1,3 +1,4 @@
+// Variáveis
 console.log('Hello world!');
 var a = 10;
 var b = 20;
@@ -10,3 +11,27 @@ console.log(nomePessoal);
 let book = "A litter prince"
 
 const PI = 3.14;
+// ----------------------------------------------------------------
+
+// Tipos de dados e operadores
+let nulo = null;
+let indefinido = undefined;
+let texto = "string";
+let numero = 123;
+let objeto1 = {};
+let objeto2 = new Object();
+let objeto3 = [];
+let objeto4 = new Array();
+let booleano = true;
+// A função typeof() te auxilia a identificar o tipo de dados
+console.log(typeof(nulo));
+console.log(typeof(indefinido));
+console.log(typeof(texto));
+console.log(typeof(numero));
+console.log(typeof(objeto1));
+console.log(typeof(objeto2));
+console.log(typeof(objeto3));
+console.log(typeof(objeto4));
+console.log(typeof(booleano));
+
+// Operadores
