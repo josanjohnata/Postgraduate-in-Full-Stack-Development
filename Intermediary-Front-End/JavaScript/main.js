1ac5786a859200eb649e0e1696440ad6cbb71966
Josan Johnata <josan.pontes@meliuz.com.br>
2023-06-13 22:49:43 -0300

    Intermediary Front end - If, switch, for, while e do/while

--- Intermediary-Front-End/JavaScript/main.js
+++ Intermediary-Front-End/JavaScript/main.js
@@ -70,3 +70,103 @@ console.log(removeSpaceBeforeAndAfterSentence.trim());
 const numbers = '1,2,3,4,5,6,7,8,9';
 const changeToArray = numbers.split(',');
 console.log(changeToArray);
+
+// If, switch, for, while and Do while
+
+// If/else
+let country = 'Brazil';
+
+if (!country === 'Brazil') {
+  console.log('Você é estrangeiro!');
+} else {
+  console.log('Você é brasileiro!');
+}
+
+let age = 22;
+
+if (age < 16) {
+  console.log('Não vota!');
+} else if (age < 18 || age > 65){
+  console.log('Voto opcional!');
+} else {
+  console.log('Voto obrigatório!');
+}
+
+// Switch/case
+const weekDay = 1;
+
+switch (weekDay) {
+  case 1:
+  console.log("Domingo")
+  break;
+
+  case 2:
+  console.log("Segunda-feira")
+  break;
+
+  case 3:
+  console.log("Terça-feira")
+  break;
+
+  case 4:
+  console.log("Quarta-feira")
+  break;
+
+  case 5:
+  console.log("Quinta-feira")
+  break;
+
+  case 6:
+  console.log("Sexta-feira")
+  break;
+
+  default:
+  console.log("Sabadouu")
+
+  break;
+}
+
+// For
+let cars = [
+  {modelo: 'Audio A3', marca: 'Audi', ano: 2020},
+  {modelo: 'Palio', marca: 'Fiat', ano: 2010},
+  {modelo: 'Corolla', marca: 'Toyota', ano: 2004}
+]
+for (let i = 0; i < 3; i++) {
+  let car = cars[i].modelo;
+  console.log(car);
+}
+
+// For/in
+let cars2 = [
+  {modelo: 'Audio A3', marca: 'Audi', ano: 2020},
+  {modelo: 'Palio', marca: 'Fiat', ano: 2010},
+  {modelo: 'Corolla', marca: 'Toyota', ano: 2004}
+]
+for (let i in cars2) {
+  console.log(cars2[i].modelo);
+}
+
+// For/of
+let cars3 = [
+  {modelo: 'Audio A3', marca: 'Audi', ano: 2020},
+  {modelo: 'Palio', marca: 'Fiat', ano: 2010},
+  {modelo: 'Corolla', marca: 'Toyota', ano: 2004}
+]
+for (let car of cars3) {
+  console.log(car.modelo);
+}
+
+// While
+let i = 1;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// Do/while
+let a = 1;
+do {
+  console.log(a);
+  a++;
+} while (a <= 10);
