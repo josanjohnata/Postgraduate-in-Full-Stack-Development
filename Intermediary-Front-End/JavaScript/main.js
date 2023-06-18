fd02efdcc363a22363761483769bd832e9e21713
Josan Johnata <josan.pontes@meliuz.com.br>
2023-06-18 16:52:35 -0300

    Front end - Criando e manipulando Arrays

--- Intermediary-Front-End/JavaScript/main.js
+++ Intermediary-Front-End/JavaScript/main.js
@@ -170,3 +170,93 @@ do {
   console.log(a);
   a++;
 } while (a <= 10);
+
+// Criando e manipulando Arrays
+// Arrays
+let values = [8, 3, 9, 0, 1, 2,];
+console.log(values[3]);
+
+for (let i = 0; i < values.length; i++) {
+  console.log('Posição:', values[i]);
+};
+
+let carsArray = [];
+carsArray[0] = "Volvo";
+carsArray[1] = "Fiat";
+carsArray[2] = "Toyota";
+
+console.log(carsArray);
+
+let motorcycles = new Array('BMW', 'Yamaha', 'Honda');
+console.log(motorcycles);
+
+// Exercício: calcular a media de todos os números de um array
+let numbersCalc = [8, 3, 9, 0, 2, 2,];
+function calculateNumbers(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total / numbers.length;
+};
+
+console.log(calculateNumbers(numbersCalc));
+
+// Exercício: localizar o maior número do array
+function findMax(numbers) {
+  let max = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  return max;
+};
+
+console.log(findMax(numbersCalc));
+
+// Exercício: localizar o nome da cidade com maior número de carácteres
+let citiesOfBrazil = ['Fortaleza', 'São Paulo', 'Rio de Janeiro', 'Alagoas']
+function findCity(cities) {
+  let max = 0;
+  for (let i = 0; i < cities.length; i++) {
+    if (cities[i].length > max) {
+      max = cities[i].length;
+      maxCity = cities[i];
+    }
+  }
+  return maxCity;
+};
+
+console.log(findCity(citiesOfBrazil));
+
+let arrTest = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log('Array com .reverse() retorna o array invertido:', arrTest.reverse());
+console.log('Array com .join() substitui o separador pelo valor passado:', arrTest.join('-'));
+console.log('Array com .shift() remove o primeiro elemento do array:', arrTest.shift());
+console.log('Array com .push(valor) adiciona um ou mais valores no final do array:', arrTest.push(99));
+console.log('Array com .unshift(valor) adiciona um ou mais valores no inicio do array:', arrTest.unshift(31));
+console.log('Array com .pop() remove o último elemento do array:', arrTest.pop());
+console.log('Array com .indexOf(numeroDaPosição) retorna a posição do valor selecionado:', arrTest.indexOf(3));
+console.log('Array com .length() retorna a quantidade de elementos do array:', arrTest.length());
+console.log('Array com .sort() ordena o array do menor para o maior e de A a Z:', arrTest.sort());
+
+// Splice()
+let arraySplice = [1, 2, 3, 'hello world', 4.14, true]
+arraySplice.splice(2, 1,);
+arraySplice.splice(2, 0, 'teste');
+console.log(arraySplice);
+
+// Exercício: Criar um array com cinco nomes, Guilherme, Samuel, John, Ester e Eide.
+// Acrescente o nome da Mônica, retire o último elemento do array,
+// encontre a posição do Samuel e troque Eide por Milk.
+let nameArray = ['Guilherme', 'Samuel', 'John', 'Ester', 'Eide'];
+console.log(nameArray);
+nameArray.push('Mônica');
+console.log(nameArray);
+nameArray.pop();
+console.log(nameArray);
+console.log('A posição do Samuel é:', nameArray.indexOf('Samuel'));
+console.log(nameArray);
+nameArray.splice(nameArray.indexOf('Eide'), 1, 'Milk');
+console.log(nameArray);
