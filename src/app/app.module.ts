86df2fb236e4da4b8cdf3b90555ea289363bb1e8
josanjohnata <josanjohnata@gmail.com>
2023-07-12 16:17:31 -0300

    Advanced Front-End - Rotas, layout Angular Material e HTML dos formulários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { ListarComponent } from './pages/listar/listar.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +22,19 @@ import { FooterComponent } from './component/footer/footer.component';
     HomeComponent,
     ListarComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
